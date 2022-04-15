2cc293b5d5d43312d903bc28290c1966596ae60b
hagarseif <gogoseif31@gmail.com>
2022-04-15 15:42:14 +0200

    update js

--- Student/JS/main.js
+++ Student/JS/main.js
@@ -1,10 +1,11 @@
 let services=document.querySelector(".services")
-let eval=document.querySelector(".eval")
+let evall=document.querySelector(".eval")
 let child=document.querySelector(".child")
 let child1=document.querySelector(".child1")
 services.addEventListener('click',()=>{
     child.classList.toggle('dis');
 })
-eval.addEventListener('click',()=>{
+evall.addEventListener('click',()=>{
     child1.classList.toggle('dis');
 })
+
